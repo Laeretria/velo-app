2123b5f16d285fbadf4fb901f90eb29a17a4858a
laeretria <laeretria@gmail.com>
2024-05-29 20:20:01 +0200

    stationlist

---
+++ src/components/StationList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Link from "next/link";
+
+const StationList = ({ stations }) => {
+  return (
+    <div>
+      <h1>Velo Stations in Antwerpen</h1>
+      <ul>
+        {stations.map((station) => (
+          <li key={station.id}>
+            <Link href={`/stations/${station.id}`}>
+              <a>{station.name}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default StationList;
