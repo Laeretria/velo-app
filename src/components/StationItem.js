20e8f19190623d12bdc2dc92ce9acf688420c8c5
laeretria <laeretria@gmail.com>
2024-05-31 00:40:52 +0200

    fixed capital address

--- src/components/StationItem.js
+++ src/components/StationItem.js
@@ -2,11 +2,17 @@ import React from "react";
 import styles from "@/styles/Home.module.css";
 
 const StationItem = ({ station }) => {
+  const capitalizeAddress = (address) => {
+    return address.toLowerCase().replace(/(^|\s)\S/g, (letter) => {
+      return letter.toUpperCase();
+    });
+  };
+
   return (
     <div>
       <h2 className={styles.stationName}>{station.name}</h2>
       <div className={styles.border}>
-        <p>{station.extra.address}</p>
+        <p>{capitalizeAddress(station.extra.address)}</p>
         <p>Free bikes: {station.free_bikes}</p>
         <p>Empty slots: {station.empty_slots}</p>
       </div>
