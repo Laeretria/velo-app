befc795917f6d8d7ab70a52e5dbf073c74525723
laeretria <laeretria@gmail.com>
2024-05-30 22:30:12 +0200

    styled stationlist

--- src/components/StationList.js
+++ src/components/StationList.js
@@ -1,15 +1,16 @@
 import React from "react";
 import Link from "next/link";
+import styles from "@/styles/Home.module.css";
 
 const StationList = ({ stations }) => {
   return (
     <div>
-      <h1>Velo Stations in Antwerpen</h1>
-      <ul>
+      <ul className={styles.stationList}>
+        <h1 className={styles.stationListH1}>Bike & Co</h1>
         {stations.map((station) => (
-          <li key={station.id}>
+          <li key={station.id} className={styles.stationItem}>
             <Link legacyBehavior href={`/stations/${station.id}`}>
-              <a>{station.name}</a>
+              <a className={styles.stationLink}>{station.name}</a>
             </Link>
           </li>
         ))}
