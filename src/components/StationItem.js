87559957681933eb2a946411adda20bb6971c00d
laeretria <laeretria@gmail.com>
2024-05-31 01:00:53 +0200

    added icons to detail page

--- src/components/StationItem.js
+++ src/components/StationItem.js
@@ -1,7 +1,11 @@
 import React from "react";
 import styles from "@/styles/Home.module.css";
+import { useRouter } from "next/router";
+import Image from "next/image";
 
 const StationItem = ({ station }) => {
+  const router = useRouter();
+
   const capitalizeAddress = (address) => {
     return address.toLowerCase().replace(/(^|\s)\S/g, (letter) => {
       return letter.toUpperCase();
@@ -13,8 +17,28 @@ const StationItem = ({ station }) => {
       <h2 className={styles.stationName}>{station.name}</h2>
       <div className={styles.border}>
         <p>{capitalizeAddress(station.extra.address)}</p>
-        <p>Free bikes: {station.free_bikes}</p>
-        <p>Empty slots: {station.empty_slots}</p>
+        <p>
+          {" "}
+          <Image
+            className={styles.icon}
+            src="/bicycle.png"
+            alt="Bike Icon"
+            width={24}
+            height={24}
+          />
+          Free bikes : {station.free_bikes}
+        </p>
+        <p>
+          {" "}
+          <Image
+            className={styles.icon}
+            src="/key-1.png"
+            alt="Slot Icon"
+            width={24}
+            height={24}
+          />
+          Empty slots : {station.empty_slots}
+        </p>
       </div>
     </div>
   );
