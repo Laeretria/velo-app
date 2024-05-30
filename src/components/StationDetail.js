e3273b06c06661fbdb669af455b85a70d58fe5b5
laeretria <laeretria@gmail.com>
2024-05-31 00:38:55 +0200

    styled detail page

--- src/components/StationDetail.js
+++ src/components/StationDetail.js
@@ -1,13 +1,14 @@
 import React from "react";
 import StationItem from "./StationItem";
 import Link from "next/link";
+import styles from "@/styles/Home.module.css";
 
 const StationDetail = ({ station }) => {
   return (
     <div>
       <StationItem station={station} />
-      <Link legacyBehavior href="/">
-        <a>Back to list</a>
+      <Link legacyBehavior href="/stations">
+        <a className={styles.back}>Home</a>
       </Link>
     </div>
   );
--- src/components/StationItem.js
+++ src/components/StationItem.js
@@ -1,12 +1,15 @@
 import React from "react";
+import styles from "@/styles/Home.module.css";
 
 const StationItem = ({ station }) => {
   return (
     <div>
-      <h2>{station.name}</h2>
-      <p>Address: {station.extra.address}</p>
-      <p>Free bikes: {station.free_bikes}</p>
-      <p>Empty slots: {station.empty_slots}</p>
+      <h2 className={styles.stationName}>{station.name}</h2>
+      <div className={styles.border}>
+        <p>{station.extra.address}</p>
+        <p>Free bikes: {station.free_bikes}</p>
+        <p>Empty slots: {station.empty_slots}</p>
+      </div>
     </div>
   );
 };
