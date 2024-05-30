bb0b393d63b7c19a6ce23cded7139fe3f4283109
laeretria <laeretria@gmail.com>
2024-05-30 22:57:50 +0200

    searchbar

--- src/components/StationList.js
+++ src/components/StationList.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import styles from "@/styles/Home.module.css";
 
 const StationList = ({ stations }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [highlightedStation, setHighlightedStation] = useState(null);
+
+  const handleSearch = (e) => {
+    const term = e.target.value;
+    setSearchTerm(term);
+
+    if (term === "") {
+      setHighlightedStation(null);
+    } else {
+      const foundStation = stations.find((station) =>
+        station.name.toLowerCase().includes(term.toLowerCase()),
+      );
+      setHighlightedStation(foundStation ? foundStation.id : null);
+    }
+  };
+
   return (
     <div>
+      <h1 className={styles.stationListH1}>Bike & Co</h1>
+      <div className={styles.searchContainer}>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={handleSearch}
+          placeholder="Search for a station..."
+          className={styles.searchInput}
+        />
+      </div>
       <ul className={styles.stationList}>
-        <h1 className={styles.stationListH1}>Bike & Co</h1>
         {stations.map((station) => (
-          <li key={station.id} className={styles.stationItem}>
+          <li
+            key={station.id}
+            className={`${styles.stationItem} ${
+              highlightedStation === station.id ? styles.highlighted : ""
+            }`}
+          >
             <Link legacyBehavior href={`/stations/${station.id}`}>
               <a className={styles.stationLink}>{station.name}</a>
             </Link>
