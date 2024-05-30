5caca14eda024f7ce41c49f902b2dd066ac6c223
laeretria <laeretria@gmail.com>
2024-05-30 15:55:47 +0200

    added startscreen + styled it

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Georama:wght@400;700&family=Poppins:wght@400;700&display=swap"
+        rel="stylesheet"
+      />
       <body>
         <Main />
         <NextScript />
---
+++ src/pages/homepage.js
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "@/styles/Home.module.css";
+
+const Homepage = () => {
+  return (
+    <>
+      <Head>
+        <title>Velo App By Vi Krumklang</title>
+        <meta name="description" content="Welcome to Velo Antwerpen" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={styles.container}>
+        <h1 className={styles.h1}>Bike & Co</h1>
+        <Image
+          src="/bike.png"
+          alt="Velo Antwerpen"
+          width={600}
+          height={400}
+          className={styles.image}
+        />
+        <div className={styles.circle}></div>
+        <h2 className={styles.h2}>
+          Discover the convenience of renting bikes from various stations across
+          Antwerp.
+        </h2>
+        <Link legacyBehavior href="/index">
+          <a className={styles.button}>Explore Stations</a>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default Homepage;
